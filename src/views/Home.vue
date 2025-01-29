d8228eb2099ef32247024c55343930c099cb06fa
kravchenko06 <161839102+kravchenko06@users.noreply.github.com>
2025-01-29 18:05:26 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,6 @@
   <div>
     <h1 class="text-h3 mb-6">Welcome to Movie Website</h1>
     <v-row>
-      <v-col cols="12" md="8">
-        <search-bar @search="handleSearch" />
-      </v-col>
-    </v-row>
-    <v-row>
       <v-col v-for="movie in featuredMovies" :key="movie.id" cols="12" sm="6" md="4">
         <movie-card :movie="movie" />
       </v-col>
@@ -15,14 +10,13 @@
 </template>
 
 <script>
-import SearchBar from '../components/SearchBar.vue';
+
 import MovieCard from '../components/MovieCard.vue';
 import { useMovieStore } from '../store';
 
 export default {
   name: 'Home',
   components: {
-    SearchBar,
     MovieCard
   },
   data() {
@@ -33,11 +27,6 @@ export default {
   created() {
     const store = useMovieStore();
     this.featuredMovies = store.movies.slice(0, 6);
-  },
-  methods: {
-    handleSearch(query) {
-      // Implement search logic
-    }
   }
 }
 </script>
